cdf834329c75fd083dc3ee32e36c20cd70af9c06
HungryWinter <github_winter@163.com>
2018-05-16 10:50:49 +0800

    xml方式配置一个最简单的springmvc webService 简化配置

--- src/main/java/com/songxd/controller/HomeController.java
+++ src/main/java/com/songxd/controller/HomeController.java
@@ -1,13 +1,14 @@
 package com.songxd.controller;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 public class HomeController {
 
-    @RequestMapping(value = "/home")
+    @RequestMapping(value = "/")
     public String index(){
-        return "home";
+        return "index";
     }
 }
